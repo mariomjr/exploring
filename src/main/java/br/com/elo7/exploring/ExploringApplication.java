4113df82e69b9a21817a478dd735de7f13e028ba
MarioJr <mariohj94@gmail.com>
2021-06-23 23:56:22 -0300

    Init config project

---
+++ src/main/java/br/com/elo7/exploring/ExploringApplication.java
@@ -0,0 +1,13 @@
+package br.com.elo7.exploring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExploringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExploringApplication.class, args);
+	}
+
+}
